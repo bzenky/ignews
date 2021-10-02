604e8c7493bee9068852af0086401f50577b296f
Zenky <bzenky@gmail.com>
2021-10-02 13:38:46 -0300

    initial structure

---
+++ src/components/Header/Index.tsx
@@ -0,0 +1,18 @@
+import { SignInButton } from '../SignInButton'
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="IgNews" />
+        <nav>
+          <a href="#" className={styles.active}>Home</a>
+          <a href="#">Posts</a>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  )
+}
