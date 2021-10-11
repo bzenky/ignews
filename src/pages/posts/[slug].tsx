a283370c218f2371190ef643c9999b51f0314517
Zenky <bzenky@gmail.com>
2021-10-11 11:45:58 -0300

    preview post, session auth router

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,7 +41,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
 
   console.log('foi', session)
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
